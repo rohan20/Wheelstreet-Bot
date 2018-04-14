bfba9c59c8baa465c360d60867764c303fc4be11
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-15 05:11:42 +0530

    setup questionsAnswers recyclerView with its adapter

--- app/src/main/java/com/rohantaneja/wheelstreetbot/ui/QuestionsActivity.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/ui/QuestionsActivity.java
@@ -2,9 +2,11 @@ package com.rohantaneja.wheelstreetbot.ui;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.util.Log;
 
 import com.rohantaneja.wheelstreetbot.R;
+import com.rohantaneja.wheelstreetbot.adapter.QuestionAnswerRecyclerViewAdapter;
 import com.rohantaneja.wheelstreetbot.databinding.ActivityQuestionsBinding;
 import com.rohantaneja.wheelstreetbot.model.QuestionAnswer;
 import com.rohantaneja.wheelstreetbot.model.QuestionsResponse;
@@ -20,7 +22,8 @@ import retrofit2.Response;
 public class QuestionsActivity extends BaseActivity {
 
     private static final String TAG = QuestionsActivity.class.getName();
-    ActivityQuestionsBinding mBinding;
+    private ActivityQuestionsBinding mBinding;
+    private QuestionAnswerRecyclerViewAdapter mQuestionAnswerRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,14 @@ public class QuestionsActivity extends BaseActivity {
     private void initUI() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_questions);
 
+        initQuestionAnswerChatView();
         fetchQuestions();
+    }
+
+    private void initQuestionAnswerChatView() {
+        mQuestionAnswerRecyclerViewAdapter = new QuestionAnswerRecyclerViewAdapter(this);
+        mBinding.chatRecyclerView.setAdapter(mQuestionAnswerRecyclerViewAdapter);
+        // TODO: 15/04/18 Add item decoration (margin)
     }
 
     private void fetchQuestions() {
@@ -44,6 +54,7 @@ public class QuestionsActivity extends BaseActivity {
                 if (response.isSuccessful()) {
                     Log.d(TAG, "Successful: " + response.body().toString());
                     List<QuestionAnswer> questionAnswerList = response.body().getData();
+                    mQuestionAnswerRecyclerViewAdapter.updateQuestionAnswerList(response.body().getData());
                     hideProgressDialog();
                 } else {
                     Log.d(TAG, "Not successful: " + response.message());
