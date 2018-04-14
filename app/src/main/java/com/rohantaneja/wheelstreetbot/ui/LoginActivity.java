cd81af59f9a7b3e0f05e5cd768b80ebcb399b775
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-14 19:05:08 +0530

    initialised UI in LoginActivity

--- app/src/main/java/com/rohantaneja/wheelstreetbot/ui/LoginActivity.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/ui/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.rohantaneja.wheelstreetbot.ui;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rohantaneja.wheelstreetbot.R;
+import com.rohantaneja.wheelstreetbot.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends BaseActivity {
+
+    private ActivityLoginBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        initUI();
+    }
+
+    private void initUI() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
     }
 }
