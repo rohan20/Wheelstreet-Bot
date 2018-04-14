71522d0e9c763555de1722908764ed77c1a42aaf
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-14 23:23:23 +0530

    added Constants class with constants for answer type

---
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/util/Constants.java
@@ -0,0 +1,14 @@
+package com.rohantaneja.wheelstreetbot.util;
+
+/**
+ * Created by rohantaneja on 14/04/18.
+ */
+
+public class Constants {
+
+    public static final int TYPE_STRING = 0;
+    public static final int TYPE_FLOAT = 1;
+    public static final int TYPE_INTEGER = 2;
+    public static final int TYPE_BOOLEAN = 3;
+
+}
