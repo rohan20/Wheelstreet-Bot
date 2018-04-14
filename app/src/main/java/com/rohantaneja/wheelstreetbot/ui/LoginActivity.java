0bcae71ddb8f913b6b1652a0a948b16f8f8848b6
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-14 19:20:49 +0530

    setup basic facebook sign in

--- app/src/main/java/com/rohantaneja/wheelstreetbot/ui/LoginActivity.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/ui/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.rohantaneja.wheelstreetbot.ui;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.Login;
+import com.facebook.login.LoginResult;
 import com.rohantaneja.wheelstreetbot.R;
 import com.rohantaneja.wheelstreetbot.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends BaseActivity {
 
+    private static final String TAG = LoginActivity.class.getName();
     private ActivityLoginBinding mBinding;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,38 @@ public class LoginActivity extends BaseActivity {
 
     private void initUI() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        initFacebookLogin();
     }
+
+    private void initFacebookLogin() {
+        mCallbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        mBinding.facebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setResult(RESULT_OK);
+                Log.d(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onCancel() {
+                setResult(RESULT_CANCELED);
+                Log.d(TAG, "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d(TAG, "onError");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
