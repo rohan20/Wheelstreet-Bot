e1a7067de2565890b4f0faba781e1fb1682965eb
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-14 23:24:58 +0530

    added method to fetch answerType of Question

--- app/src/main/java/com/rohantaneja/wheelstreetbot/model/Question.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/model/Question.java
@@ -2,6 +2,7 @@ package com.rohantaneja.wheelstreetbot.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.rohantaneja.wheelstreetbot.util.Constants;
 
 /**
  * Created by rohantaneja on 14/04/18.
@@ -37,6 +38,17 @@ public class Question {
 
     private String getDataType() {
         return dataType;
+    }
+
+    public int getAnswerType() {
+        if (dataType.equals("boolean"))
+            return Constants.TYPE_BOOLEAN;
+        if (dataType.equals("integer"))
+            return Constants.TYPE_INTEGER;
+        if (dataType.equals("float"))
+            return Constants.TYPE_FLOAT;
+
+        return Constants.TYPE_STRING;
     }
 
     public void setDataType(String dataType) {
