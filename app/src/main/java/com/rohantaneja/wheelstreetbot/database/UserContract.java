0db40d797d5d93237fb0d314b9c8796ba01fd025
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-15 20:27:24 +0530

    added UserContract class for column names

---
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/database/UserContract.java
@@ -0,0 +1,31 @@
+package com.rohantaneja.wheelstreetbot.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by rohantaneja on 15/04/18.
+ */
+
+public class UserContract {
+
+    //to prevent initialisation of the UserContract class, mark the constructor as private
+    private UserContract() {
+
+    }
+
+    public static class UserEntry implements BaseColumns {
+        public static final String TABLE_NAME = "user_table";
+        public static final String COLUMN_ID = "user_id";
+        public static final String COLUMN_NAME = "user_name";
+        public static final String COLUMN_MOBILE = "user_mobile";
+        public static final String COLUMN_EMAIL = "user_email";
+        public static final String COLUMN_GENDER = "user_gender";
+        public static final String COLUMN_AGE = "user_age";
+        public static final String COLUMN_AVATAR_URL = "user_url";
+        public static final String COLUMN_AVATAR_PATH = "user_avatar_path";
+        public static final String COLUMN_IS_AVATAR_FROM_PATH = "user_is_avatar_from_path";
+    }
+
+}
+
+
