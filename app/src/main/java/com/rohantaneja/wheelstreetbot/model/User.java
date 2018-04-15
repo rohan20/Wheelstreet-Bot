b74910c49cab72a2b17dcaf82fff3dd07842f624
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-15 06:22:35 +0530

    added method to calculate age from dob

--- app/src/main/java/com/rohantaneja/wheelstreetbot/model/User.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/model/User.java
@@ -1,5 +1,11 @@
 package com.rohantaneja.wheelstreetbot.model;
 
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by rohantaneja on 15/04/18.
  */
@@ -40,7 +46,7 @@ public class User {
     }
 
     public int getAge() {
-        return age;
+        return calculateAgeFromDob();
     }
 
     public String getGender() {
@@ -77,5 +83,45 @@ public class User {
 
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
+    }
+
+    private int calculateAgeFromDob() {
+
+        int age = 0;
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar dateOfBirthCalendar = Calendar.getInstance();
+
+        try {
+            dateOfBirthCalendar.setTime(sdf.parse(birthday));
+        } catch (ParseException e) {
+            Log.d(getClass().getName(), "Invalid DOB, can't calculate age.");
+        }
+
+        Calendar todayCalendar = Calendar.getInstance();
+        int currentYear = todayCalendar.get(Calendar.YEAR);
+        int dobYear = dateOfBirthCalendar.get(Calendar.YEAR);
+        age = currentYear - dobYear;
+
+        // if dob is month or day is behind today's month or day, reduce age by 1
+        int currentMonth = todayCalendar.get(Calendar.MONTH);
+        int dobMonth = dateOfBirthCalendar.get(Calendar.MONTH);
+
+        if (dobMonth > currentMonth) {
+            // this year shouldn't be counted!
+            age--;
+        } else if (dobMonth == currentMonth) {
+            // same month? compare dates
+            int curDay = todayCalendar.get(Calendar.DAY_OF_MONTH);
+            int dobDay = dateOfBirthCalendar.get(Calendar.DAY_OF_MONTH);
+
+            if (dobDay > curDay) {
+                // birthday not here yet, this year shouldn't be counted!
+                age--;
+            }
+        }
+
+        return age;
     }
 }
