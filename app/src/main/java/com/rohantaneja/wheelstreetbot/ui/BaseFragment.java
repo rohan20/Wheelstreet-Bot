e36c101214b7575694725d6010cc863fbcf59840
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-15 15:03:17 +0530

    added BaseFragment

---
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/ui/BaseFragment.java
@@ -0,0 +1,29 @@
+package com.rohantaneja.wheelstreetbot.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rohantaneja.wheelstreetbot.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BaseFragment extends Fragment {
+
+    private Toast toast;
+
+    protected void showToast(String message) {
+        if (getActivity() != null && message != null) {
+            if (toast != null)
+                toast.cancel();
+            toast = Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+}
