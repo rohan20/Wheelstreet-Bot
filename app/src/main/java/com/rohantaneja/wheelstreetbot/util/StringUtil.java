aaf9497177823d7d5c61a05b8041a2d83677691a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-16 04:29:14 +0530

    added StringUtil class with method to check if string is null or empty

---
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.rohantaneja.wheelstreetbot.util;
+
+/**
+ * Created by rohantaneja on 16/04/18.
+ */
+
+public class StringUtil {
+
+    public static boolean isNullOrEmpty(String data) {
+        return !(data == null || data.isEmpty());
+    }
+
+}
