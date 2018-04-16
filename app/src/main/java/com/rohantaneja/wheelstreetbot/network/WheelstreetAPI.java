5d8e374a8fb8c8f04d152df1b5857e4f619f9257
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-16 14:03:32 +0530

    added POST method for submitting survey

--- app/src/main/java/com/rohantaneja/wheelstreetbot/network/WheelstreetAPI.java
+++ app/src/main/java/com/rohantaneja/wheelstreetbot/network/WheelstreetAPI.java
@@ -1,9 +1,13 @@
 package com.rohantaneja.wheelstreetbot.network;
 
 import com.rohantaneja.wheelstreetbot.model.QuestionsResponse;
+import com.rohantaneja.wheelstreetbot.model.pojo.request.SubmittedSurveyRequest;
+import com.rohantaneja.wheelstreetbot.model.pojo.response.SubmittedSurveyResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by rohantaneja on 15/04/18.
@@ -13,5 +17,8 @@ public interface WheelstreetAPI {
 
     @GET("questions")
     Call<QuestionsResponse> getQuestions();
+
+    @POST("answers")
+    Call<SubmittedSurveyResponse> submitSurvey(@Body SubmittedSurveyRequest surveyRequest);
 
 }
